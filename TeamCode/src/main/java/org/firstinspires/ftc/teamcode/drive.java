ba949e3c69528200f2e3d2262a8d4d835a57be7d
wykzimme <kianazim@gmail.com>
2024-10-14 19:00:18 -0600

    hi

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive.java
@@ -0,0 +1,35 @@
+ package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "drive")
+public class drive extends OpMode {
+
+    DcMotor topleft, topright;
+    DcMotor backleft, backright;
+
+
+    @Override
+    public void init() {
+        topleft = hardwareMap.get(DcMotor.class, "topleft");
+        backleft = hardwareMap.get(DcMotor.class, "backleft");
+        topright = hardwareMap.get(DcMotor.class, "topright");
+        backright = hardwareMap.get(DcMotor.class, "backright");
+
+
+
+
+    }
+
+    @Override
+    public void loop() {
+        topleft.setPower(-gamepad1.right_stick_y + gamepad1.left_stick_x * 1.1 + gamepad1.right_stick_xx);
+        backleft.setPower(gamepad1.right_stick_y + -gamepad1.left_stick_x * 1.1 + -gamepad1.left_stick_x);
+        topright.setPower(-gamepad1.right_stick_y + gamepad1.left_stick_x * 1.1 + -gamepad1.left_stick_x);
+        backright.setPower(gamepad1.right_stick_y + -gamepad1.left_stick_x * 1.1 + gamepad1.left_stick_x);
+
+    }
+}
